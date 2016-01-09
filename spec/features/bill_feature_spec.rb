5171205d2c62eeffac2e2bbcdfb0d80736b6d799
Andrew Stelmach <andrew_stelmach@hotmail.com>
2016-01-09 13:06:29 +0000

    wrote first feature test

--- spec/features/bill_feature_spec.rb
+++ spec/features/bill_feature_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 feature 'Bill Features' do
   context 'when on bill show page' do
+    before(:each){ visit '/' }
+  end
 
+  it 'has a title' do
+    expect(page).to have_content 'Your Bill'
   end
 end
