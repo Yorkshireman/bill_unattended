b8918ba969c7d8e4ad3a1cffea42216740e7388c
Andrew Stelmach <andrew_stelmach@hotmail.com>
2016-01-09 23:57:26 +0000

    TestJson module created to hold a replica of what GetBillData returns (a json object parsed into a hash). Http request is now stubbed in the GetBillData spec.

--- lib/services/get_bill_data.rb
+++ lib/services/get_bill_data.rb
@@ -2,6 +2,6 @@ require 'http'
 
 class GetBillData
   def self.call
-    HTTP.get("http://safe-plains-5453.herokuapp.com/bill.json").to_s
+    JSON.parse(HTTP.get("http://safe-plains-5453.herokuapp.com/bill.json"))
   end
 end
--- spec/bill_data_getter_spec.rb
+++ spec/bill_data_getter_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 require_relative '../lib/services/get_bill_data'
+require_relative './support/test_json'
 
 describe GetBillData do
-  it 'retrieves a string from an endpoint' do
-    response = GetBillData.call
-    expect(response).to be_a String
+  let(:subject){ GetBillData }
+
+  it 'provides a hash of data from an endpoint' do
+    allow(subject).to receive(:call).and_return TestJson.data 
+    response = subject.call
+    expect(response).to eq TestJson.data
+    expect(response).to be_a Hash
   end
 end
