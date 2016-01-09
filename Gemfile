6f39d30e1aa8c6c088c3048994db8ce964684f09
Andrew Stelmach <andrew_stelmach@hotmail.com>
2016-01-09 12:50:32 +0000

    about to add Sinatra

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'http'
+gem 'json'
 
 group :test do
   gem 'guard-rspec', require: false
--- spec/bill_data_getter_spec.rb
+++ spec/bill_data_getter_spec.rb
@@ -3,6 +3,7 @@ require_relative '../lib/services/get_bill_data'
 
 describe GetBillData do
   it 'retrieves a string from an endpoint' do
-    expect(GetBillData.call).to be_a String
+    response = GetBillData.call
+    expect(response).to be_a String
   end
 end
