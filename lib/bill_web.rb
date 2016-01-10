76e1b2423b2e5bfdc787908dbb3bfd7d7f70141a
Andrew Stelmach <andrew_stelmach@hotmail.com>
2016-01-10 22:04:59 +0000

    assets path configured and being served successfully locally. Haven't tried in deployment yet.

--- lib/bill_web.rb
+++ lib/bill_web.rb
@@ -3,6 +3,7 @@ require_relative './bill'
 require_relative './views/helpers/view_helpers'
 
 class BillWeb < Sinatra::Base
+  set :public_folder, File.dirname(__FILE__) + '/assets'
   include ViewHelpers
 
   get '/' do
