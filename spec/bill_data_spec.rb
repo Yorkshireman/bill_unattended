97aaf3e17ca36821a42ccb0e973d023e1fabb87e
Andrew Stelmach <andrew_stelmach@hotmail.com>
2016-01-10 01:16:40 +0000

    stubbed http request in Bill Spec

--- spec/bill_data_spec.rb
+++ spec/bill_data_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 require 'bill'
+require_relative '../lib/services/get_bill_data'
 require_relative './support/test_data_hash'
 
 describe Bill do
   let(:test_data_hash){ TestDataHash.data }
+  before(:each){ allow(GetBillData).to receive(:call).and_return test_data_hash }
 
-  it 'provides a hash of data from an endpoint' do
-    # allow(subject).to receive(:call).and_return test_data_hash 
+  it 'provides a hash of bill data' do
     expect(subject.data).to eq test_data_hash
     expect(subject.data).to be_a Hash
   end
